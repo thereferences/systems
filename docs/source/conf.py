60a7c7e0c6738f216267fca7b57094870b942813
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-08 23:29:31 +0000

    Add docs/source/conf.py

---
+++ docs/source/conf.py
@@ -0,0 +1,99 @@
+"""
+conf.py
+
+Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+Project information
+https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+"""
+
+import sphinx_book_theme
+import sphinx_theme
+
+
+'''
+Basic
+'''
+project = 'Mathematical Systems'
+copyright = '2023, greyhypotheses'
+author = 'greyhypotheses'
+release = '0.1'
+
+
+'''
+General configuration
+https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+Sphinx extension modules. Extensions are either Sphinx extensions, named 'sphinx.ext.*', or custom ones.
+'''
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.githubpages'
+]
+
+
+'''
+Templates
+'''
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+'''
+Options for HTML output
+https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+'''
+
+html_theme = 'sphinx_rtd_theme'
+
+html_static_path = ['_static']
+
+html_css_files = [
+    'css/style.css'
+]
+
+html_js_files = ['https://code.jquery.com/jquery-3.7.0.min.js',
+                 'https://code.highcharts.com/stock/highstock.js',
+                 'https://code.highcharts.com/stock/modules/data.js',
+                 'https://code.highcharts.com/stock/modules/exporting.js',
+                 'https://code.highcharts.com/stock/modules/export-data.js',
+                 'https://code.highcharts.com/stock/modules/accessibility.js',
+                 'https://code.highcharts.com/highcharts.js',
+                 'https://code.highcharts.com/modules/networkgraph.js',
+                 'js/latex.js'
+]
+
+html_theme_options = {
+    'color_scheme': '',
+    'canonical_url': 'https://membranes.github.io/systems',
+    'analytics_id': '',
+    'style_external_links': False,
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False,
+    'github_url': 'https://www.github.com/membranes/systems',
+    'logo_mobile': '_static/logo.svg'
+}
+
+html_logo = '_static/logo.svg'
+
+html_context = {
+    'landing_page': {
+        'menu': [
+            {'title': 'Introduction',
+             'url': 'introduction/introduction.html'},
+            {'title': 'Project',
+             'url': 'project/project.html'},
+            {'title': 'Search',
+             'url': 'search.html'}
+        ]
+    }
+}
+
