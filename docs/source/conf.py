4199b6fbbbfadb745d79df73809bfcda92caabf2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-12 18:21:48 +0000

    Update docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,19 +46,17 @@ exclude_patterns = []
 Options for HTML output
 https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-Index Logo / Left Panel Logo
-html_logo = '_static/logo.svg'
 '''
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'revitron_sphinx_theme'
 
-html_static_path = ['_static']
+html_static_path: list[str] = ['_static']
 
-html_css_files = [
+html_css_files: list[str] = [
     'css/style.css'
 ]
 
-html_js_files = ['https://code.jquery.com/jquery-3.7.0.min.js',
+html_js_files: list[str] = ['https://code.jquery.com/jquery-3.7.0.min.js',
                  'https://code.highcharts.com/stock/highstock.js',
                  'https://code.highcharts.com/stock/modules/data.js',
                  'https://code.highcharts.com/stock/modules/exporting.js',
@@ -70,6 +68,7 @@ html_js_files = ['https://code.jquery.com/jquery-3.7.0.min.js',
 ]
 
 html_theme_options = {
+    'color_scheme': '',
     'canonical_url': 'https://membranes.github.io/systems',
     'analytics_id': '',
     'style_external_links': False,
@@ -77,8 +76,12 @@ html_theme_options = {
     'sticky_navigation': True,
     'navigation_depth': 4,
     'includehidden': True,
-    'titles_only': False
+    'titles_only': False,
+    'github_url': 'https://www.github.com/membranes/systems',
+    'logo_mobile': '_static/logo.svg'
 }
+
+html_logo = '_static/logo.svg'
 
 html_context = {
     'landing_page': {
